736fe7c2656b200bdc02b336c7b119cf527e7797
Ben GREGORY <61834092+BenGregory23@users.noreply.github.com>
2024-07-30 11:32:27 +0200

    added ideas

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
-
-<h1 class="text-4xl">Hello World</h1>
-
-<Button>test</Button>
+<div class="flex flex-col w-full h-screen items-center justify-center">
+    <h1 class="text-3xl font-medium">Projet Test de la caisse</h1>
+    <span>Idées</span>
+    <ul>
+       
+        <li>Faire l'accueil</li>
+        <li>Faire la page de menu/écrans</li>
+        <li>Faire la page de paiement</li>
+        <li>Puis essayer des fonctionnalités de capacitor</li>
+    </ul>
+</div>
 
 
 
